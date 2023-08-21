25f7c2b7793a33a4d156f6ba0c6d009c51c313e3
Jiun Jye Yim <jyim@u90z06s04.42kl.edu.my>
2023-08-21 18:32:41 +0800

    WIP:Parsing

--- includes/parse.hpp
+++ includes/parse.hpp
@@ -6,29 +6,30 @@
 /*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 16:00:42 by jyim              #+#    #+#             */
-/*   Updated: 2023/08/18 18:22:43 by jyim             ###   ########.fr       */
+/*   Updated: 2023/08/21 17:23:38 by jyim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.hpp"
 #include <fstream>
+#include <map>
 
-class Server {
-	private:
-		int					_port;
-		int					_serverfd;
-		struct sockaddr_in	_server_addr;
-		std::vector<int>	_client_sockets;
+//class Server {
+//	private:
+//		int					_port;
+//		int					_serverfd;
+//		struct sockaddr_in	_server_addr;
+//		std::vector<int>	_client_sockets;
 
-		Server( void );
-		int init( void );
+//		Server( void );
+//		int init( void );
 
-	public:
-		Server( int port );
-		~Server( void );
+//	public:
+//		Server( int port );
+//		~Server( void );
 
-		int run( void );
-};
+//		int run( void );
+//};
 
 //server {  //start of server block
 //    listen       <port>; // can insert multiple ports
@@ -39,18 +40,22 @@ class Server {
 //     allowedMethods <security>; //restrict what a url is able to send to server. https://coldbox.ortusbooks.com/the-basics/event-handlers/http-method-security
 //} //end of server block
 
+//static enum StringValue { evNotDefined,
+//                          evStringValue1,
+//                          evStringValue2,
+//                          evStringValue3,
+//                          evEnd };
+						  
 struct Location
 {
 	std::string					path;
 	std::vector<std::string>	allowedMethods;
-	std::string					root;
 	
 };
 
-
 struct ServerConfig
 {
-	std::vector<int>	listen;
+	int					listen;
 	std::string			name;
 	std::string			root;
 	std::vector<Location>	locations;
@@ -58,13 +63,26 @@ struct ServerConfig
 
 class Config {
 	private:
-		std::vector<ServerConfig>	_servers;
+		std::vector<ServerConfig>	_ports;
 	public:
 		Config( void );
 		Config(char *av);
 		~Config( void );
-
-		void parse(std::fstream &file);
-		std::vector<ServerConfig> get_servers(void) { return _servers; }
-		std::string	getstring(std::fstream &file);
+		enum	serverBlock{
+			notDefined = 0,
+			listen = 1,
+			server_name = 2,
+			root = 3,
+			location = 4
+		};
+		void	initEnum(std::map<std::string, serverBlock> &s_mapStringValues);
+		void	parse(std::string &file);
+		void	parseServerBlock(std::istringstream &iss);
+		//void	parseListen(std::istringstream &iss);
+		//void	parseServerName(std::istringstream &iss);
+		//void	parseRoot(std::istringstream &iss);
+		//void	parseLocation(std::istringstream &iss);
+		//std::vector<ServerConfig> get_servers(void) { return _ports; }
+		std::string	getstring(char *av);
+		//void	printfServerConfig(void);
 };
--- src/parse.cpp
+++ src/parse.cpp
@@ -6,7 +6,7 @@
 /*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 16:01:43 by jyim              #+#    #+#             */
-/*   Updated: 2023/08/18 18:22:42 by jyim             ###   ########.fr       */
+/*   Updated: 2023/08/21 17:21:12 by jyim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,41 +17,109 @@
 
 Config::Config(){
 	//USE DEFAULT CONF FILE if there are no files specified
+	std::cout << "Default constructor" << std::endl;
 }
 
 Config::~Config( void ){
 	std::cout << "Deconstruct Config" << std::endl;
 }
-
-std::string	Config::getstring(std::fstream &file){
-
-	
+						  
+std::string	Config::getstring(char *av){
+	std::ifstream fsFile(av);
+	std::string ret;
+	if (!fsFile){
+		std::cout << "File not found" << std::endl;
+		return ("");
+	}
+	std::string line;
+	while(std::getline(fsFile, line)) {
+		std::cout << line << '\n';
+		ret += line + "\n";
+	}
+	return ret;
 }
 
-void	Config::parse(std::fstream &file)
-{
-	std::string str = config.getstring(file); // write function to write whole file? or change to parse perline
-	std::istringstream iss(str);
+void	Config::initEnum(std::map<std::string, serverBlock> &s_mapStringValues){
+	s_mapStringValues["listen"] = listen;
+	s_mapStringValues["server_name"] = server_name;
+	s_mapStringValues["root"] = root;
+	s_mapStringValues["location"] = location;
+}
+
+void	Config::parseServerBlock(std::istringstream &iss){
+	std::map<std::string, serverBlock> s_mapStringValues;
+	Config::initEnum(s_mapStringValues);
+	ServerConfig server;
+	int end = 0;
+
+	std::cout << "IN CONFIG::PARSESERVERBLOCK" << std::endl;
 	do {
+    	std::string subs;
+        // Get the word from the istringstream
+        iss >> subs;
+        // Print the word fetched
+        // from the istringstream
+        std::cout << "(Start2)" << subs << "(End2)" << std::endl;
+		// exit server block
+		if (subs.find("}") != -1){
+			std::cout << "Close Bracket found" << std::endl;
+			end = 1;
+		}
+		// parse to correct variable
+		switch (s_mapStringValues[subs]){
+		case listen:
+			//server.listen = ;
+			//parseListen(iss);
+			std::cout << "Parse Listen" << std::endl;
+			break ;
+		case server_name:
+			//parseServerName(iss);
+			std::cout << "Server Name" << std::endl;
+			break ;
+		default:
+			std::cout << "Wrong string" << std::endl;
+		}	
+ 
+    } while (end == 0);
+		//this->_ports.push_back(server);
+}
+
+void	Config::parse(std::string &file){
+	//if (file.empty())
+	std::istringstream iss(file);
+	do {
+		std::cout << "IN CONFIG::PARSE" << std::endl;
     	std::string subs;
         // Get the word from the istringstream
         iss >> subs;
  
         // Print the word fetched
         // from the istringstream
-        std::cout << subs << std::endl;
+        std::cout << "(Start)" << subs << "(End)" << std::endl;
 
 		// parse to correct variable
+		if (subs.find("server") != -1)
+			Config::parseServerBlock(iss);
  
     } while (iss);
 }
 
-Config::Config(char *av)
-{
-	//std::string serverConfig = av;
-	std::fstream file;
-	file.open(av, std::ios::in);
-	if (!file)
-		std::cout << "File not found" << std::endl;
-	Config::parse(file);
+Config::Config(char *av){
+	std::ifstream file(av);
+	std::string serverConfig = Config::getstring(av);
+	Config::parse(serverConfig);
+}
+
+
+int main(int argc, char **argv){
+	Config S(argv[1]);
+	//std::vector<ServerConfig> ports = Server.get_servers();
+	//int ports_size = ports.size();
+
+	//std::vector<ServerConfig>::iterator iter = ports.begin();	
+	//for(iter; iter < ports.end(); iter++)
+	//{
+    //	std::cout << (*iter).name << std::endl;
+	//}
+	std::cout << std::endl;
 }
