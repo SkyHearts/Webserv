5e31c98cd71f88e578c7d2aad994bed6a5dec1f0
Wong Hong You <wonghongyou@gmail.com>
2023-08-24 00:16:04 +0800

    rewriting logic

--- includes/server.hpp
+++ includes/server.hpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <sys/select.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <netdb.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <vector>
 
+typedef struct Port {
+	int			port;
+	std::string	name;
+}				Port;
+
 class Server {
 	private:
-		int					_port;
-		int					_serverfd;
-		struct sockaddr_in	_server_addr;
-		std::vector<int>	_client_sockets;
+		fd_set						_readfds, _writefds;
+		std::vector<int>			_serverports;
+		std::vector<int>			_serverfds;
+		std::vector<sockaddr_in>	_serveraddrs;
+		Port						_port_info;
+
 
 		Server( void );
 		int init( void );
+		int loop( void );
+
+		void error( std::string errmsg, bool exitbool = false );
 
 	public:
 		Server( int port );
