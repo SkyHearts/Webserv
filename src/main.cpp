3c84326eafeb1c420345e8b12e80e57662728879
Wong Hong You <wonghongyou@gmail.com>
2023-08-31 16:37:47 +0800

    Made it so that if ./webserv is called without config file specified, a default file will run

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "server.hpp"
+#include <fstream>
 
 int parse_config( std::string config_file ) {
 	std::string extension = ".conf";
@@ -12,17 +13,22 @@ int parse_config( std::string config_file ) {
 		std::cerr << RED << "Error: Unable to open configuration file" << CLEAR << std::endl;
 		return 1;
 	}
+
+	return 0;
 }
 
-int main() {
+int main( int ac, char **av ) {
 	Server server;
 
-	if (ac > 2) {
+	if (ac < 0 || ac > 2) {
 		std::cerr << RED << "Error: Invalid usage\nUsage: ./webserv [file.conf]" << CLEAR << std::endl;
 		return 1;
 	}
 
-	std::string config_file = av[1];
+	std::string config_file = "server.conf";
+	if (ac == 2)
+		config_file = av[1];
+
 	if (parse_config(config_file))
 		return 1;
 
