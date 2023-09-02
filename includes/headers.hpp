2a19db7b041c40786a5fe1b98ce3244e2de3976b
Mars <almasnorazman2803@outlook.com>
2023-09-02 17:51:14 +0800

    organised headers

--- includes/headers.hpp
+++ includes/headers.hpp
@@ -3,8 +3,11 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <sys/select.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <netdb.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <vector>
 #include <map>
@@ -12,6 +15,7 @@
 
 #define RED "\033[31m"
 #define GREEN "\033[32m"
+#define GREEN_BOLD "\033[32;1m"
 #define YELLOW "\033[33m"
 #define BLUE "\033[34m"
 #define CLEAR "\033[0m"
