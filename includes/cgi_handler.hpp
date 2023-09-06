7014f48e1434b7670c73386f0186e8766ef90d7e
Jiun Jye Yim <jiunjye.yim@gmail.com>
2023-09-06 19:12:00 +0800

    WIP:cgi

--- includes/cgi_handler.hpp
+++ includes/cgi_handler.hpp
@@ -6,7 +6,7 @@
 /*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 15:53:55 by jyim              #+#    #+#             */
-/*   Updated: 2023/09/04 17:18:20 by jyim             ###   ########.fr       */
+/*   Updated: 2023/09/06 19:06:08 by jyim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,42 @@
 
 #include "headers.hpp"
 
-typedef struct env
-{
-	std::string SERVER_SOFTWARE;
-	int 		SERVER_PORT;
-	std::string REQUEST_METHOD;
-	std::string PATH_INFO;
-	std::string PATH_TRANSLATED;
-	std::string HTTP_REFERER;
-	std::string HTTP_ACCEPT;
-} WebEnv;
+//typedef struct env
+//{
+//	std::string SERVER_SOFTWARE;
+//	int 		SERVER_PORT;
+//	std::string REQUEST_METHOD;
+//	std::string PATH_INFO;
+//	std::string PATH_TRANSLATED;
+//	std::string HTTP_REFERER;
+//	std::string HTTP_ACCEPT;
+//} WebEnv;
 
 class cgi_handler
 {
 	private:
-	WebEnv _structEnv;
-	char **_structEnv_env;
+	//WebEnv _structEnv;
+	char **_env;
 	public:
 	cgi_handler();
 	~cgi_handler();
 
-	//Get funct;
-	int getServerPort(void) {return _structEnv.SERVER_PORT;}
-	std::string getServerSoftware(void) {return _structEnv.SERVER_SOFTWARE;}
-	std::string getRequestMethod(void) {return _structEnv.REQUEST_METHOD;}
-	std::string getPathInfo(void) {return _structEnv.PATH_INFO;}
-	std::string getPathTranslated(void) {return _structEnv.PATH_TRANSLATED;}
-	std::string getHTTPReferer(void) {return _structEnv.HTTP_REFERER;}
-	std::string getHTTPAccept(void) {return _structEnv.HTTP_ACCEPT;}
+	//Get funct WebEnv;
+	//int getServerPort(void) {return _structEnv.SERVER_PORT;}
+	//std::string getServerSoftware(void) {return _structEnv.SERVER_SOFTWARE;}
+	//std::string getRequestMethod(void) {return _structEnv.REQUEST_METHOD;}
+	//std::string getPathInfo(void) {return _structEnv.PATH_INFO;}
+	//std::string getPathTranslated(void) {return _structEnv.PATH_TRANSLATED;}
+	//std::string getHTTPReferer(void) {return _structEnv.HTTP_REFERER;}
+	//std::string getHTTPAccept(void) {return _structEnv.HTTP_ACCEPT;}
+
+	//Get funct _env;
+	char** getEnv(void) { return _env; }
 
 	void create_env(void);
+	void add_env(std::string envVar);
 	void setEnv(char** env);
-	void exec_cgi(WebEnv env);
+	std::string exec_cgi(std::string path, char **argv);
+	void delEnv(char **env);
+	void copyEnv(char **dest, char **src);
 };
--- src/cgi_handler.cpp
+++ src/cgi_handler.cpp
@@ -6,17 +6,67 @@
 /*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 17:08:33 by jyim              #+#    #+#             */
-/*   Updated: 2023/09/04 17:10:59 by jyim             ###   ########.fr       */
+/*   Updated: 2023/09/06 19:09:07 by jyim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cgi_handler.hpp"
 
 cgi_handler::cgi_handler(){
-	//USE DEFAULT CONF FILE if there are no files specified
+	_env = NULL;
 	std::cout << "Default constructor" << std::endl;
 }
 
 cgi_handler::~cgi_handler( void ){
 	std::cout << "Deconstruct Config" << std::endl;
+	//delete env
+	for(int i = 0; i < (sizeof(_env) / sizeof(char*)); ++i) {
+        delete[] _env[i];   
+    }
+    //Free the array of pointers
+    delete[] _env;
+}
+
+void cgi_handler::add_env(std::string envVar){
+	int envSize;
+	char** tmp_env;
+	if (_env == NULL){
+		_env = new char*[2];
+		_env[0] = new char[envVar.length() + 1];
+		//_env[0] = envVar.c_str();
+		strcpy(_env[0], envVar.c_str());
+		_env[1] = NULL;
+	}
+	envSize = (sizeof(_env) / sizeof(char*));
+	tmp_env = new char*[envSize + 1];
+	copyEnv(tmp_env, _env);
+	strcpy(tmp_env[envSize - 1], envVar.c_str());
+	tmp_env[envSize] = NULL;
+	delEnv(_env);
+	_env = tmp_env;
+
+}
+
+void cgi_handler::create_env(void){
+	add_env("SERVER_SOFTWARE = WebServ");
+	add_env("SERVER_PORT =" + );
+}
+
+std::string cgi_handler::exec_cgi(std::string path, char **argv){
+	pid_t cgi;
+	std::string response;
+	cgi = fork();
+	if (cgi == 0)
+	{
+		execve(path.c_str(), argv, this->_env);
+	}
+	return (response);
+}
+
+void cgi_handler::delEnv(char **env){
+	for(int i = 0; i < (sizeof(env) / sizeof(char*)); ++i) {
+		delete[] env[i];   
+    }
+    //Free the array of pointers
+    delete[] env;
 }
