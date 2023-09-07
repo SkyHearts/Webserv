2f57f71767fffac05cfd579b7970aef3272594f2
Jiun Jye Yim <jiunjye.yim@gmail.com>
2023-09-07 16:10:02 +0800

    Chore:Readme update and SampleCGI moved

--- includes/cgi_handler.hpp
+++ includes/cgi_handler.hpp
@@ -6,7 +6,7 @@
 /*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 15:53:55 by jyim              #+#    #+#             */
-/*   Updated: 2023/09/06 19:06:08 by jyim             ###   ########.fr       */
+/*   Updated: 2023/09/07 13:15:42 by jyim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,31 +25,40 @@
 //	std::string HTTP_ACCEPT;
 //} WebEnv;
 
+//To help return response?
+class cgi_response
+{
+	protected:
+	std::string _header;
+	public:
+	//Get func
+	std::string getHeader(void) {return _header;}
+};
+
 class cgi_handler
 {
 	private:
 	//WebEnv _structEnv;
 	char **_env;
+	char **_arg;
 	public:
 	cgi_handler();
 	~cgi_handler();
-
-	//Get funct WebEnv;
-	//int getServerPort(void) {return _structEnv.SERVER_PORT;}
-	//std::string getServerSoftware(void) {return _structEnv.SERVER_SOFTWARE;}
-	//std::string getRequestMethod(void) {return _structEnv.REQUEST_METHOD;}
-	//std::string getPathInfo(void) {return _structEnv.PATH_INFO;}
-	//std::string getPathTranslated(void) {return _structEnv.PATH_TRANSLATED;}
-	//std::string getHTTPReferer(void) {return _structEnv.HTTP_REFERER;}
-	//std::string getHTTPAccept(void) {return _structEnv.HTTP_ACCEPT;}
-
+	
 	//Get funct _env;
 	char** getEnv(void) { return _env; }
 
-	void create_env(void);
-	void add_env(std::string envVar);
-	void setEnv(char** env);
-	std::string exec_cgi(std::string path, char **argv);
+	//Set _env
+	void createEnv(void);
+	void addEnv(std::string envVar);
 	void delEnv(char **env);
-	void copyEnv(char **dest, char **src);
+	void reassginEnv(char **dest, char **src);
+	
+	//Set argv, arg[0] = path to cgi
+	void createArg(void);
+	
+	//Execute in child process using execve
+	void execCGI(std::string path, char **argv);
 };
+
+int getCharDArraySize(char** array);
